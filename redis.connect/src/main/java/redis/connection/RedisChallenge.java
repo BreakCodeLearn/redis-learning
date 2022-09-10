326ccbc99aabe8463c91d29bbc159c40b6e16ccc
“BreakCodeLearn” <himanshu.adhw@gmail.com>
2022-09-10 07:45:48 +0000

    Initial commit

---
+++ redis.connect/src/main/java/redis/connection/RedisChallenge.java
@@ -0,0 +1,27 @@
+package redis.connection;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisChallenge {
+
+  public RedisChallenge() {
+  }
+
+  public static void main(final String[] args) throws Exception {
+    // source-db
+    Jedis jedis = new Jedis("172.16.22.21", 16783);
+    jedis.auth("himanshu");
+    jedis.flushAll();
+    for (int i = 0; i <= 100; i++) {
+      jedis.rpush("myKey", Integer.toString(i));
+    }
+    jedis.close();
+
+    //replica-db
+    Jedis jedisRep = new Jedis("172.16.22.23", 11137);
+    for (int i = 100; i >= 1; i--) {
+      System.out.println(jedisRep.lindex("myKey", i));
+    }
+    jedisRep.close();
+  }
+}
---
+++ redis.connect/src/test/java/redis/connection/RedisChallengeTest.java
@@ -0,0 +1,18 @@
+package redis.connection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class RedisChallengeTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testRedisChallenge() {
+        assertTrue(true);
+    }
+}
