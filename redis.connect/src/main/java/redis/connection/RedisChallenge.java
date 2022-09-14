d1bcaa6429d0804b672c40f662379edafefe4d31
“BreakCodeLearn” <himanshu.adhw@gmail.com>
2022-09-14 05:08:39 +0000

    Rename variables and methods

--- redis.connect/src/main/java/redis/connection/RedisChallenge.java
+++ redis.connect/src/main/java/redis/connection/RedisChallenge.java
@@ -8,13 +8,13 @@ public class RedisChallenge {
   }
 
   public static void main(final String[] args) {
-    String hKey = "HimKey";
+    String hKey = "HimanRedisKey";
     RedisChallenge red = new RedisChallenge();
-    red.connectAndInsertData(hKey);
-    red.printData(hKey);
+    red.connectAndInsertDataToRedis(hKey);
+    red.printDataFromRedis(hKey);
   }
 
-  private void connectAndInsertData(String himKey) {
+  private void connectAndInsertDataToRedis(String HimanRedisKey) {
     // source-db
     Jedis jedis = new Jedis("172.16.22.21", 16783);
     try {
@@ -23,25 +23,26 @@ public class RedisChallenge {
     } catch (Exception e) {
       System.out.println("Connection to source-db: FAILED: " + e);
     }
-    if (jedis.exists(himKey)) {
-      System.out
-          .println(himKey + " : KEY already exist in the souce-db, so deleting it for the sake of this exercise.");
-      jedis.del(himKey);
+    if (jedis.exists(HimanRedisKey)) {
+      System.out.println(
+          HimanRedisKey + " : KEY already exist in the souce-db, so deleting it for the sake of this exercise.");
+      jedis.del(HimanRedisKey);
+      jedis.flushAll();
     }
     for (int i = 1; i <= 100; i++) {
-      jedis.zadd(himKey, i, Integer.toString(i));
+      jedis.zadd(HimanRedisKey, i, Integer.toString(i));
     }
     System.out.println("New data insertion to the redis source-db is complete!");
     jedis.close();
   }
 
-  private void printData(String himKey) {
+  private void printDataFromRedis(String HimanRedisKey) {
     // replica-db
     try {
       Jedis jedisReplica = new Jedis("172.16.22.23", 11137);
       System.out.println("Connection to replica-db: SUCCESS: " + "PING-" + jedisReplica.ping());
       System.out.println("Printing data from the redis replica-db in the reverse order now...");
-      System.out.println(jedisReplica.zrevrange(himKey, 0, -1));
+      System.out.println(jedisReplica.zrevrange(HimanRedisKey, 0, -1));
       jedisReplica.close();
     } catch (Exception e) {
       System.out.println("Connection to replica-db: FAILED: " + e);
